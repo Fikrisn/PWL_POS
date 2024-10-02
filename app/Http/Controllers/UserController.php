f411fb0ae8e427c60b839e602198cb054ef1a138
Moch Fikri Setiawan <mixchannel1307@gmail.com>
2024-10-02 20:08:53 +0700

    Jobsheet 6 - praktikum 2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -231,16 +231,22 @@ class UserController extends Controller
             $users->where('level_id',$request->level_id);
         }
         return DataTables::of($users)
-            // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
-            ->addIndexColumn()
+            ->addIndexColumn()            // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
             ->addColumn('aksi', function ($user) { // menambahkan kolom aksi
-                $btn = '<a href="' . url('/user/' . $user->user_id) . '" class="btn btn-info btnsm">Detail</a> ';
-                $btn .= '<a href="' . url('/user/' . $user->user_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a> ';
-                $btn .= '<form class="d-inline-block" method="POST" action="' .
-                    url('/user/' . $user->user_id) . '">'
-                    . csrf_field() . method_field('DELETE') .
-                    '<button type="submit" class="btn btn-danger btn-sm" onclick="return
-confirm(\'Apakah Anda yakit menghapus data ini?\');">Hapus</button></form>';
+//                 $btn = '<a href="' . url('/user/' . $user->user_id) . '" class="btn btn-info btnsm">Detail</a> ';
+//                 $btn .= '<a href="' . url('/user/' . $user->user_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a> ';
+//                 $btn .= '<form class="d-inline-block" method="POST" action="' .
+//                     url('/user/' . $user->user_id) . '">'
+//                     . csrf_field() . method_field('DELETE') .
+//                     '<button type="submit" class="btn btn-danger btn-sm" onclick="return
+// confirm(\'Apakah Anda yakit menghapus data ini?\');">Hapus</button></form>';
+
+            $btn = '<button onclick="modalAction(\''.url('/user/' . $user->user_id .
+            '/show_ajax').'\')" class="btn btn-info btn-sm">Detail</button> ';
+            $btn .= '<button onclick="modalAction(\''.url('/user/' . $user->user_id .
+            '/edit_ajax').'\')" class="btn btn-warning btn-sm">Edit</button> ';
+            $btn .= '<button onclick="modalAction(\''.url('/user/' . $user->user_id .
+            '/delete_ajax').'\')" class="btn btn-danger btn-sm">Hapus</button> ';
                 return $btn;
             })
             ->rawColumns(['aksi']) // memberitahu bahwa kolom aksi adalah html
@@ -384,5 +390,49 @@ confirm(\'Apakah Anda yakit menghapus data ini?\');">Hapus</button></form>';
             ]);
         }
         redirect('/');
+    }
+
+    public function edit_ajax(string $id){
+        $user = UserModel::find($id);
+        $level = LevelModel::select('level_id', 'level_nama')->get();
+
+        return view('user.edit_ajax',['user' => $user, 'level' => $level]);
+    }
+
+    public function update_ajax(Request $request, $id){
+        if ($request->ajax() || $request->wantsJson()){
+            $rules = [
+                'level_id' => 'required|integer',
+                'username' => 'required|max:20|unique:m_user,username,'.$id.'user_id',
+                'nama' => 'required|max:100',
+                'password' => 'nullable|min:6|max:20'
+            ];
+            $validator = Validator::make($request->all(), $rules);
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => 'false',
+                    'message' => 'Validasi gagal',
+                    'msgField' => $validator->errors(),
+                ]);
+            }
+
+            $chek = UserModel::find($id);
+            if ($chek){
+                if (!$request->field('password')){
+                    $request->request->remove('password');
+                }
+                $chek->update($request->all());
+                return response()->json([
+                    'status' =>  true,
+                    'message' => 'Data User Berhasil Diupdate'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Data User Tidak Ditemukan'
+                ]);
+            }
+        }
+        return redirect('/');
     }
 }
