59ab1817ee982bfb5238f5b175d10605ba9dd838
Moch Fikri Setiawan <mixchannel307@gmail.com>
2024-11-01 00:33:05 +0700

    Jobsheet 11 - praktikum 1 (RESTFUL API 2)

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\UserModel;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -14,18 +15,23 @@ class RegisterController extends Controller
             'username' => 'required',
             'nama' => 'required',
             'password' => 'required|min:5|confirmed',
-            'level_id' => 'required'
+            'level_id' => 'required',
+            'image' => 'required|iamge|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
 
         if($validator->fails()) {
             return response()->json($validator->errors(), 422);
         }
 
+        $image = $request->file('image');
+        $image->store('images', 'public');
+
         $user = UserModel::create([
             'username' => $request->username,
             'nama' => $request->nama,
             'password' => bcrypt($request->password),
-            'level_id' => $request->level_id
+            'level_id' => $request->level_id,
+            'image' => $image->hashName(),
         ]);
 
         if($user) {
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,38 +5,64 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
+//     public function getJWTIdentifier() {
+//         return $this->getKey();
+//     }
+    
+//     public function getJWTCustomClaims() {
+//         return [];
+//     }
+
+//     protected $table = 'm_user';        // Mendefinisikan nama tabel yang digunakan oleh model ini
+//     protected $primaryKey = 'user_id';  //Mendefinisikan primary key dari tabel yang digunakan
+
+//     protected $fillable = ['level_id', 'profile_image', 'username', 'nama', 'password'];
+
+//     protected $hidden = ['password']; 
+//     protected $casts = ['password' => 'hashed'];
+
+//     public function level():BelongsTo {
+//         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+//     }
+
+//     public function geRoleName(): string {
+//         return $this->level->level_nama;
+//     }
+
+//     public function hasRole($role): bool {
+//         return $this->level->level_kode == $role;
+//     }
+
+//     public function getRole() {
+//         return $this->level->level_kode;
+//     }
+
     public function getJWTIdentifier() {
         return $this->getKey();
     }
-    
+
     public function getJWTCustomClaims() {
         return [];
     }
 
-    protected $table = 'm_user';        // Mendefinisikan nama tabel yang digunakan oleh model ini
-    protected $primaryKey = 'user_id';  //Mendefinisikan primary key dari tabel yang digunakan
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
+    protected $fillable = ['level_id', 'image', 'username', 'nama', 'password'];
 
-    protected $fillable = ['level_id', 'profile_image', 'username', 'nama', 'password'];
-
-    protected $hidden = ['password']; 
-    protected $casts = ['password' => 'hashed'];
-
-    public function level():BelongsTo {
+    public function level(): BelongsTo {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 
-    public function geRoleName(): string {
-        return $this->level->level_nama;
+    protected function image():Attribute {
+        return Attribute::make(
+            get: fn($image) => url('/storage/posts/'. $image)
+        );
     }
 
-    public function hasRole($role): bool {
-        return $this->level->level_kode == $role;
-    }
-
-    public function getRole() {
-        return $this->level->level_kode;
-    }
 }
+
